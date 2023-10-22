3388769b842175b152c2c6f3d8169c1acf675459
semir709 <selmansemir555@gmail.com>
2023-10-22 21:27:24 +0200

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Category, Home, List, Movie, Navigation, Search } from "./components";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path={"/*"} element={<Home />} />
+        <Route path={"/movie/:id"} element={<Movie />} />
+        <Route path={"/search/:input"} element={<Search />} />
+        <Route path={"/category/:catg"} element={<Category />} />
+        <Route path={"/list/:list"} element={<List />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Category } from "./Category";
+export { default as List } from "./List";
+export { default as Search } from "./Search";
+export { default as Navigation } from "./Navigation";
+export { default as Movie } from "./Movie";
