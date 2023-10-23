d9f2c807212497b3d5e8ea19f5242e69381be904
semir709 <selmansemir555@gmail.com>
2023-10-23 14:40:47 +0200

    added the search logic

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,4 @@ export { default as category_icon } from "./category.svg";
 export { default as statistic_icon } from "./statistic.svg";
 export { default as filter_icon } from "./filter.svg";
 export { default as search_icon } from "./search.svg";
+export { default as close_icon } from "./close.svg";
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,47 @@
-import React from "react";
-import { search_icon } from "../assets";
+import React, { useEffect, useRef } from "react";
+import { close_icon, search_icon } from "../assets";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const input = useRef(null);
+  const navigate = useNavigate();
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    const timeID = setTimeout(() => {
+      if (searchTerm) {
+        navigate(`/search/${searchTerm}`);
+      } else navigate("/");
+    }, 1000);
+
+    return () => clearTimeout(timeID);
+  }, [searchTerm]);
+
+  const onClose = () => {
+    navigate("/");
+    setSearchTerm("");
+    input.current.focus();
+  };
+
   return (
-    <div className="flex items-center bg-white rounded-md">
+    <div className="flex items-center bg-white rounded-md ">
       <input
+        ref={input}
         type="text"
         id="search"
         placeholder="Search..."
         className="text-custom-black outline-none ps-2 rounded-md"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
       />
-      <label htmlFor="search" className="ms-3 hover:cursor-pointer pe-2">
-        <img src={search_icon} alt="search_icon" />
+      <label htmlFor="search" className="ms-3 hover:cursor-pointer  w-[25px]">
+        {searchTerm ? (
+          <img src={close_icon} alt="search_icon" onClick={onClose} />
+        ) : (
+          <img src={search_icon} alt="search_icon" />
+        )}
       </label>
     </div>
   );
