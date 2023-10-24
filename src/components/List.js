57fc929276454ffaed6f110d7190c2190613704a
semir709 <selmansemir555@gmail.com>
2023-10-24 10:44:27 +0200

    toggle animation drop down

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,17 @@
 const List = ({ Icon, text, list, toggle, setToggle }) => {
   return (
     <>
-      {toggle && (
-        <div className="w-full h-screen bg-red-300 fixed top-0 left-0">
-          <p onClick={() => setToggle(false)}>Close</p>
-          {list.map((val) => (
-            <p>{val}</p>
-          ))}
-        </div>
-      )}
+      <div
+        className={`w-full h-screen bg-red-300 fixed top-0 -translate-y-full transition-all left-0 ${
+          toggle && "translate-y-0"
+        }`}
+      >
+        <p onClick={() => setToggle(false)}>Close</p>
+        {list.map((val) => (
+          <p>{val}</p>
+        ))}
+      </div>
+
       <div className="flex items-center group" onClick={() => setToggle(true)}>
         <Icon className="group-hover:fill-custom-pink" />
         <span className="ms-2 group-hover:text-custom-pink">{text}</span>
