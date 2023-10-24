360978aaecaf281863a792c7f9bf7a538ca6a179
semir709 <selmansemir555@gmail.com>
2023-10-24 10:39:59 +0200

    added the toggle for selection

--- src/components/List.js
+++ src/components/List.js
@@ -1,7 +1,20 @@
-import React from "react";
-
-const List = () => {
-  return <div>List</div>;
+const List = ({ Icon, text, list, toggle, setToggle }) => {
+  return (
+    <>
+      {toggle && (
+        <div className="w-full h-screen bg-red-300 fixed top-0 left-0">
+          <p onClick={() => setToggle(false)}>Close</p>
+          {list.map((val) => (
+            <p>{val}</p>
+          ))}
+        </div>
+      )}
+      <div className="flex items-center group" onClick={() => setToggle(true)}>
+        <Icon className="group-hover:fill-custom-pink" />
+        <span className="ms-2 group-hover:text-custom-pink">{text}</span>
+      </div>
+    </>
+  );
 };
 
 export default List;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Statistic_icon, logo, Category_icon, Logo } from "../assets";
-
-import SearchBar from "./SearchBar";
+import { List, SearchBar } from "../components/index";
 
 const Navigation = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <>
       <div className="bg-custom-black py-4 px-custom-side text-white flex items-center justify-between">
@@ -17,12 +17,13 @@ const Navigation = () => {
           <div className="ms-5">
             <ul className="flex items-center">
               <li className="mx-3 cursor-pointer ">
-                <div className="flex items-center group">
-                  <Category_icon className="group-hover:fill-custom-pink" />
-                  <span className="ms-2 group-hover:text-custom-pink">
-                    Category
-                  </span>
-                </div>
+                <List
+                  text={"Category"}
+                  Icon={Category_icon}
+                  list={["Action", "Horror", "Drama"]}
+                  toggle={toggle}
+                  setToggle={setToggle}
+                />
               </li>
               <li className="mx-3 cursor-pointer">
                 <div className="flex items-center group">
